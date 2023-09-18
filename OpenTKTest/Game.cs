ad27e0c6f751d6ed1c30bef97f63b4736ba88a7b
Turcanu Cristian <tsoorckanukris@gmail.com>
2023-09-18 18:18:06 +0300

    added EBO

--- OpenTKTest/Game.cs
+++ OpenTKTest/Game.cs
@@ -8,13 +8,22 @@ namespace OpenTKTest;
 public class Game : GameWindow
 {
     float[] vertices = {
-        -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-        0.5f, -0.5f, 0.0f, //Bottom-right vertex
-        0.0f,  0.5f, 0.0f  //Top vertex
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    
+    uint[] indices = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
 
-    int VertexBufferObject;
+    private int VertexBufferObject;
+    private int VertexArrayObject;
+    private int ElementBufferObject;
     private Shader _shader;
+    
 
     public Game(int width, int height, string title) : base(GameWindowSettings.Default,
         new NativeWindowSettings() { Size = (width, height), Title = title }) { }
@@ -34,12 +43,22 @@ public class Game : GameWindow
     {
         base.OnLoad();
 
-        GL.ClearColor(0.2f, 1f, 1f, 1.0f);
+        VertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(VertexArrayObject);
+        
+        GL.ClearColor(0.2f, 0.5f, 0.6f, 1.0f);
         VertexBufferObject = GL.GenBuffer();
         GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
         GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
-        //Code goes here
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+        
+        
+        ElementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
         
         _shader = new Shader("shader.vert", "shader.frag");
     }
@@ -49,10 +68,25 @@ public class Game : GameWindow
         base.OnRenderFrame(e);
 
         GL.Clear(ClearBufferMask.ColorBufferBit);
+        
+        // GL.UseProgram();
+        GL.BindVertexArray(VertexArrayObject);
+        someOpenGLFunctionThatDrawsOurTriangle();
+
 
         //Code goes here.
 
         SwapBuffers();
+    }
+
+    private void someOpenGLFunctionThatDrawsOurTriangle()
+    {
+        
+        _shader.Use();
+        GL.BindVertexArray(VertexArrayObject);
+        // GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+
     }
 
     protected override void OnResize(ResizeEventArgs e)
