615a60be96898268ff28bc0d69a76a3093a74827
Turcanu Cristian <tsoorckanukris@gmail.com>
2023-09-24 21:35:15 +0300

    don't load too many models

--- OpenTKTest/Source/Essentials/ModernGame.cs
+++ OpenTKTest/Source/Essentials/ModernGame.cs
@@ -46,14 +46,15 @@ public class ModernGame : GameWindow
         GL.ClearColor(0.2f, 0.5f, 0.6f, 1.0f);
         
 
-        _models = new[] { Prefabs.GetArrow(), Prefabs.GetArrow(), Prefabs.GetArrow() };
+        _models = new[] { Prefabs.GetArrow() };
+        // _models = new[] { Prefabs.GetArrow(), Prefabs.GetArrow(), Prefabs.GetArrow() };
         // _models = new[] { ModelLoader.LoadQuad() };
         // _models = new[] { Prefabs.GetTexturedQuad() };
         // _models = new[] { Prefabs.GetArrow(), Prefabs.GetQuad() };
         // _models[0].Color = Vector4.One;
 
-        _models[1].Rotation += new Vector3(0, 90, 0);
-        _models[2].Rotation += new Vector3(0, 0, 90);
+        // _models[1].Rotation += new Vector3(0, 90, 0);
+        // _models[2].Rotation += new Vector3(0, 0, 90);
 
         foreach (var model in _models)
         {
